2aae237dc1ec5896c8fc4a7db20e3ac816a03ce7
Reubber <reubbers@alu.ufc.br>
2020-08-27 18:49:32 -0300

    add Video List

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import React, {Fragment} from 'react';
 import styled from 'styled-components'
+import GlobalStyle from './theme/globalStyle';
+import Play from '@components/play'
 
 import 'normalize.css'
 import 'milligram'
-import GlobalStyle from './theme/globalStyle';
+
 
 function App() {
 
@@ -16,14 +18,37 @@ function App() {
 `
   const Footer = styled.footer`
     height: ${footerHeight};
-    background: #333
+    background: #333;
 `
   const Main = styled.main`
     min-height: calc(100% - ${headerHeight} - ${footerHeight});
-    background: pink
+    background: pink;
 `
   const Container = styled.div`
     height: 100%
+`
+  const VideosList = styled.div`
+    display: flex;
+    flex-wrap: wrap;
+`
+  const Video = styled.section`
+    flex: 1 1 300px;
+    margin: 0 5px 5px; 
+`
+  const VideoThumb = styled.div`
+    display: flex;
+    align-items: center;
+    border: 1px solid #999;
+    height: 150px;
+    justify-content: center;
+`
+const VideoTitle = styled.h2`
+    font-size: 18px;
+`
+const PlayStyled = styled(Play)`
+    fill: #999;
+    width: 150px;
+    height: 50px;
 `
   return (
     <Fragment>
@@ -34,7 +59,16 @@ function App() {
         </Header>
       
         <Main>
-          Conteudo
+          <VideosList>
+            {Array.from({ length:10 }).map((item, index) => (
+              <Video key={index}>
+                <VideoThumb>
+                  <PlayStyled />
+                </VideoThumb> 
+                <VideoTitle>Titulo do video</VideoTitle>
+              </Video>
+            ))}
+          </VideosList>
         </Main>
 
         <Footer>
