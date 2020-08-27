146cda119cc80b5309e336710fd7fb9fa4ef25cf
Reubber <reubbers@alu.ufc.br>
2020-08-27 16:56:57 -0300

    base structure with style components

--- src/App.js
+++ src/App.js
@@ -1,14 +1,49 @@
-import React from 'react';
+import React, {Fragment} from 'react';
+import styled from 'styled-components'
 
 import 'normalize.css'
 import 'milligram'
+import GlobalStyle from './theme/globalStyle';
 
 function App() {
+
+  const headerHeight='60px'
+  const footerHeight='30px'
+
+  const Header = styled.header`
+    height: ${headerHeight};
+    background: #333
+`
+  const Footer = styled.footer`
+    height: ${footerHeight};
+    background: #333
+`
+  const Main = styled.main`
+    min-height: calc(100% - ${headerHeight} - ${footerHeight});
+    background: pink
+`
+  const Container = styled.div`
+    height: 100%
+`
   return (
-   
-    <h1>Hello world</h1>
-       
+    <Fragment>
+      <GlobalStyle />
+      <Container>
+        <Header>
+          <h1>Reactflix</h1>
+        </Header>
+      
+        <Main>
+          Conteudo
+        </Main>
+
+        <Footer>
+          &copy;2020
+        </Footer>
+      </Container> 
+    </Fragment>
   )
+
 }
 
 export default App;
---
+++ src/theme/globalStyle.js
@@ -0,0 +1,9 @@
+import { createGlobalStyle } from 'styled-components';
+ 
+const GlobalStyle = createGlobalStyle`
+  html, body, div#root {
+    height: 100%;
+  }
+`;
+ 
+export default GlobalStyle;
