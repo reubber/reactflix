f98c0ae1d68746855940e3004041b4185e311cd3
Reubber <reubbers@alu.ufc.br>
2020-09-15 23:57:18 -0300

    read info from database

--- src/index.js
+++ src/index.js
@@ -4,9 +4,16 @@ import { Provider } from 'react-redux';
 import App from './App';
 import configureStore from './redux-flow/configure-store' 
 import * as serviceWorker from './serviceWorker';
+import { db } from './config/firebase'
 
 
 const store = configureStore()
+
+const videos = db.ref('videos')
+
+videos.on('value', (snapshot) => {
+  console.log('snapshot', snapshot.val())
+})
 
 ReactDOM.render(
   <React.StrictMode>
