2b0c3230433e0d4e028a6f91197aec72af571f6e
Reubber <reubbers@alu.ufc.br>
2020-09-19 20:20:47 -0300

    m3#a82

--- src/index.js
+++ src/index.js
@@ -10,9 +10,20 @@ import { db } from './config/firebase'
 const store = configureStore()
 
 const videos = db.ref('videos')
+const setVideo = videos.push() //para usar o metodo set do firebase deve-se da um push para inicir nova propriedade no db
+const anyVideo = videos.child(345)
+
+
 
 videos.on('value', (snapshot) => {
   console.log('snapshot', snapshot.val())
+}, (error) => {
+  console.log('error:', error)
+})
+
+anyVideo.update({         //metodo nao destrutivo
+  id: "002",
+  title: "ｈｅｙ　ｙo"
 })
 
 ReactDOM.render(
